3f439b3786eddea4e5033263d8dbb104d93e32e2
lislis <lislis@redqueen.local>
2026-07-13 11:30:19 +0200

    update main

--- first-game-2/main.gd
+++ first-game-2/main.gd
@@ -1,14 +1,29 @@
 extends Node2D
 
 @export var score: int
-@export var score_label: String = "Ducks clicked: "
+@export var score_label: String
+
+var duck_scene = load("res://duck.tscn")
+var display
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	score = 0
+	score_label = $Control/Clicks.text
 	$Control/Clicks.text = score_label + "%s" % score
+	
+	display = DisplayServer.screen_get_size()
+	
+	var new_duck = duck_scene.instantiate()
+	var duck_size = new_duck.get_child(1).get_texture().get_size()
+	var rand_x = randi_range(duck_size.x, display.x - duck_size.x)
+	var rand_y = randi_range(duck_size.y, display.y - duck_size.x)
+	new_duck.position = Vector2(rand_x, rand_y)
+	
+	$DuckManager.add_child(new_duck)
+	
 	for duck in $DuckManager.get_children():
-		#print(duck.name)
+		print(duck.name)
 		duck.clicked.connect(_on_duck_clicked)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
